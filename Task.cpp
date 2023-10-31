e203777cd8479a66e5ac51d6725299df429caf38
Ahmedlokma <ahmedlokma22@gmail.com>
2023-10-31 18:40:07 +0200

    merged featureone into main

--- Task.cpp
+++ Task.cpp
@@ -5,6 +5,14 @@ using namespace std;
 class Task
 {
 public:
+    // Function that calculates average of a specific vector
+    double getAverage(std::vector<int> vector)
+    {
+        double average = 0;
+        int sum = getSum(vector);
+        average = (double)sum / vector.size();
+        return average;
+    }
     // Function calculates sum of a specifc vector
     int getSum(std::vector<int> vector)
     {
@@ -22,7 +30,9 @@ int main()
     Task task;
     std::vector<int> vector = {10, 2, 3, 4, 5, 6, 7, 8, 9, 1};
     int sumResult = task.getSum(vector);
+    double average = task.getAverage(vector);
     std::cout << "Sum Result: " << sumResult << std::endl;
+    std::cout << "Average: " << average << std::endl;
 
     return 0;
 }
