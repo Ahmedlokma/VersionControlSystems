5a8280e0b7f30e2e34f58a198c3a88e45124c7e8
Ahmedlokma <ahmedlokma22@gmail.com>
2023-10-31 18:42:17 +0200

    Minimum Element function added

--- Task.cpp
+++ Task.cpp
@@ -5,6 +5,19 @@ using namespace std;
 class Task
 {
 public:
+    // function returns minimum element
+    int getMinimum(std::vector<int> vector)
+    {
+        int min = vector[0];
+        for (int i = 1; i < vector.size(); i++)
+        {
+            if (min > vector[i])
+            {
+                min = vector[i];
+            }
+        }
+        return min;
+    }
     // Function calculates sum of a specifc vector
     int getSum(std::vector<int> vector)
     {
@@ -22,7 +35,9 @@ int main()
     Task task;
     std::vector<int> vector = {10, 2, 3, 4, 5, 6, 7, 8, 9, 1};
     int sumResult = task.getSum(vector);
+    int minimumElement = task.getMinimum(vector);
     std::cout << "Sum Result: " << sumResult << std::endl;
+    std::cout << "Minimum Element: " << minimumElement << std::endl;
 
     return 0;
 }
